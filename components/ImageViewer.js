87855e6d523303c677d2767d770adcab8a98642e
milind-nair <nairmilind3@gmail.com>
2023-03-24 15:55:36 +0530

    created basic functionality of app

---
+++ components/ImageViewer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+const ImageViewer = ({ placeholderImageSource, selectedImage }) => {
+  const imageSource =
+    selectedImage !== null ? { uri: selectedImage } : placeholderImageSource;
+  return <Image source={imageSource} style={styles.image}></Image>;
+};
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
+export default ImageViewer;
